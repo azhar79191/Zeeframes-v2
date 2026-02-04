056841cf6971640e1db8658c1358f3633621ca22
Muhammad Azhar <wattooazhar758@gmail.com>
2026-02-04 18:55:03 +0500

    decreased the size of icons

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -663,7 +663,7 @@ export const Mobile = () => {
 
 export const Frame = () => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none">
             <path d="M5 12C5 9.191 5 7.787 5.674 6.778C5.96591 6.34106 6.34106 5.96591 6.778 5.674C7.787 5 9.19 5 12 5C14.81 5 16.213 5 17.222 5.674C17.6589 5.96591 18.0341 6.34106 18.326 6.778C19 7.787 19 9.19 19 12C19 14.81 19 16.213 18.326 17.222C18.0341 17.6589 17.6589 18.0341 17.222 18.326C16.213 19 14.81 19 12 19C9.19 19 7.787 19 6.778 18.326C6.34106 18.0341 5.96591 17.6589 5.674 17.222C5 16.213 5 14.81 5 12Z" stroke="#F3FE00" stroke-width="1.5" />
             <path d="M12.857 9L10 12H14L11.143 15" stroke="#F3FE00" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
